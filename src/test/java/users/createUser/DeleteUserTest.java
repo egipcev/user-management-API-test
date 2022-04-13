50e170a62ca6b50e6858a1575e182abfdc3f2937
oleksii yehiptsev <oleksii.yehiptsev@skf.com>
2022-04-13 14:07:52 +0300

    add delete existing user test

--- src/test/java/users/createUser/DeleteUserTest.java
+++ src/test/java/users/createUser/DeleteUserTest.java
@@ -1,16 +1,16 @@
 package users.createUser;
 
-import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import users.BasicTest;
 import users.User;
 
 public class DeleteUserTest extends BasicTest {
 
-    private String id;
+    private static String id;
 
-    @BeforeEach
-    public void createUser() {
+    @BeforeAll
+    public static void createUser() {
         String username = "user-" + getRandomString();
         String password = "password";
         String email = username + "@mail.com";
@@ -24,6 +24,12 @@ public class DeleteUserTest extends BasicTest {
     public void deleteUser() {
         restClient.deleteUser(id)
                 .assertThat().statusCode(200);//add db check
+    }
+
+    @Test
+    public void deleteNotExistingUser() {
+        restClient.deleteUser("0")
+                .assertThat().statusCode(400);
     }
 
 }
