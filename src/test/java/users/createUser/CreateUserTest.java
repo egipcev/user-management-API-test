fa6443b7878a0172c6e0320d82cab0ad91de3950
oleksii yehiptsev <oleksii.yehiptsev@skf.com>
2022-04-13 13:19:58 +0300

    add negative create account test

--- src/test/java/users/createUser/CreateUserTest.java
+++ src/test/java/users/createUser/CreateUserTest.java
@@ -55,4 +55,31 @@ public class CreateUserTest extends BasicTest {
                 .assertThat().body("account.profile.key1", equalTo("value1"))
                 .assertThat().body("account.profile.key2", equalTo("value2"));
     }
+
+    @Test
+    public void testCreateExistingUserAllFieldsNegative() {
+        String username = "user-" + getRandomString();
+        String password = "password";
+        String email = username + "@mail.com";
+        Map<String, String> profile = new HashMap<>();
+        profile.put("key1", "value1");
+        profile.put("key2", "value2");
+        String id = UUID.randomUUID().toString();
+        restClient.createUser(new User(
+                username, password, id, email, profile))
+                .assertThat().statusCode(200)
+                .assertThat().body("account.username", equalToIgnoringCase(username))
+                .assertThat().body("account.email", equalToIgnoringCase(email))
+                .assertThat().body("account.id", equalTo(id))
+                .assertThat().body("account.created", notNullValue())
+                .assertThat().body("account.updated", notNullValue())
+                .assertThat().body("account.verification_date", notNullValue())
+                .assertThat().body("account.verified", equalTo(false))
+                .assertThat().body("account.profile.key1", equalTo("value1"))
+                .assertThat().body("account.profile.key2", equalTo("value2"));
+        restClient.createUser(new User(
+                username, password, id, email, profile))
+                .assertThat().statusCode(400)
+                .assertThat().body("detail", equalTo("account already exists"));
+    }
 }
